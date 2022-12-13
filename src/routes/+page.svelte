6d71e8de4dd8f6c99cfc680407438e734c2c0bf8
chamo112 <chamo.011270@fmail.com>
2022-12-14 01:00:36 +0900

    Add Tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let count = 0;
+
+	const handleClick = () => {
+		count++;
+	};
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
